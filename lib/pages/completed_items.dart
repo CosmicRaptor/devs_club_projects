c758c158a29bc66eadf0e669eac3af6f82a9f693
CosmicRaptor <60008842+CosmicRaptor@users.noreply.github.com>
2023-10-04 11:39:18 +0530

    added SingleChildScrollView to allow scrolling through the tasks.

--- lib/pages/completed_items.dart
+++ lib/pages/completed_items.dart
@@ -54,117 +54,120 @@ class _CompletedItemsState extends State<CompletedItems> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.grey[200],
+    return SingleChildScrollView(
+      physics: BouncingScrollPhysics(),
+      child: Scaffold(
+          backgroundColor: Colors.grey[200],
 
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            showDialog(context: context,
-                builder: (_) {
-                  taskNameController.clear();
-                  taskDescriptionController.clear();
-                  return StatefulBuilder(
-                      builder: (context, setState) {
-                        return SimpleDialog(
-                          title: Text('New task'),
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.symmetric(vertical: 8.0,
-                                  horizontal: 12.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  TextField(
-                                    maxLength: 50,
-                                    maxLengthEnforcement: MaxLengthEnforcement
-                                        .enforced,
-                                    controller: taskNameController,
-                                    decoration: InputDecoration(
-                                        border: OutlineInputBorder(),
-                                        hintText: 'Title'
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              showDialog(context: context,
+                  builder: (_) {
+                    taskNameController.clear();
+                    taskDescriptionController.clear();
+                    return StatefulBuilder(
+                        builder: (context, setState) {
+                          return SimpleDialog(
+                            title: Text('New task'),
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.symmetric(vertical: 8.0,
+                                    horizontal: 12.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    TextField(
+                                      maxLength: 50,
+                                      maxLengthEnforcement: MaxLengthEnforcement
+                                          .enforced,
+                                      controller: taskNameController,
+                                      decoration: InputDecoration(
+                                          border: OutlineInputBorder(),
+                                          hintText: 'Title'
+                                      ),
                                     ),
-                                  ),
-                                  SizedBox(height: 10.0,),
-                                  TextField(
-                                    maxLength: 300,
-                                    maxLengthEnforcement: MaxLengthEnforcement
-                                        .enforced,
-                                    controller: taskDescriptionController,
-                                    decoration: InputDecoration(
-                                        border: OutlineInputBorder(),
-                                        hintText: 'Description'
+                                    SizedBox(height: 10.0,),
+                                    TextField(
+                                      maxLength: 300,
+                                      maxLengthEnforcement: MaxLengthEnforcement
+                                          .enforced,
+                                      controller: taskDescriptionController,
+                                      decoration: InputDecoration(
+                                          border: OutlineInputBorder(),
+                                          hintText: 'Description'
+                                      ),
                                     ),
-                                  ),
-                                  Row(
-                                    children: [
-                                      SizedBox(width: 3.0,),
-                                      DropdownButton(
-                                          value: dropDownValue,
-                                          items: values.map((String value) {
-                                            return DropdownMenuItem(
-                                              value: value,
-                                              child: Text(value),
-                                            );
-                                          }).toList(),
-                                          onChanged: (String? newValue) {
+                                    Row(
+                                      children: [
+                                        SizedBox(width: 3.0,),
+                                        DropdownButton(
+                                            value: dropDownValue,
+                                            items: values.map((String value) {
+                                              return DropdownMenuItem(
+                                                value: value,
+                                                child: Text(value),
+                                              );
+                                            }).toList(),
+                                            onChanged: (String? newValue) {
+                                              setState(() {
+                                                dropDownValue = newValue!;
+                                              });
+                                            }
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: [
+                                        TextButton(
+                                          onPressed: () {
                                             setState(() {
-                                              dropDownValue = newValue!;
+                                              //print(taskDescriptionController.text);
+                                              //print(dropDownValue);
+                                              addData(Tasks(
+                                                  Name: taskNameController.text,
+                                                  Description: taskDescriptionController
+                                                      .text,
+                                                  Tag: dropDownValue,
+                                                  Status: false));
+                                              getInitialData();
+                                              Navigator.pop(context);
                                             });
-                                          }
-                                      ),
-                                    ],
-                                  ),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.end,
-                                    children: [
-                                      TextButton(
-                                        onPressed: () {
-                                          setState(() {
-                                            //print(taskDescriptionController.text);
-                                            //print(dropDownValue);
-                                            addData(Tasks(
-                                                Name: taskNameController.text,
-                                                Description: taskDescriptionController
-                                                    .text,
-                                                Tag: dropDownValue,
-                                                Status: false));
-                                            getInitialData();
-                                            Navigator.pop(context);
-                                          });
-                                        }, child: Text('Submit'),
-                                      ),
-                                    ],
-                                  )
-                                ],
-                              ),
-                            )
-                          ],
-                        );
-                      }
-                  );
-                });
-          },
-          child: Icon(Icons.add),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-              children: taskList.map((task) =>
-                  TaskCard(
-                    task: task,
-                    delete: () {
-                      setState(() {
-                        taskList.remove(task);
-                        deleteData(task.Name);
-                      });
-                    },
-                    edit: () {
-                      setState(() {
-                        getInitialData();
-                      });
-                    },
-                  )).toList()
+                                          }, child: Text('Submit'),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          );
+                        }
+                    );
+                  });
+            },
+            child: Icon(Icons.add),
           ),
-        )
+          body: SingleChildScrollView(
+            child: Column(
+                children: taskList.map((task) =>
+                    TaskCard(
+                      task: task,
+                      delete: () {
+                        setState(() {
+                          taskList.remove(task);
+                          deleteData(task.Name);
+                        });
+                      },
+                      edit: () {
+                        setState(() {
+                          getInitialData();
+                        });
+                      },
+                    )).toList()
+            ),
+          )
+      ),
     );
   }
 }
